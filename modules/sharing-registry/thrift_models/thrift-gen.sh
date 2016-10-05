8afc7ea2e14bc1258033d163f351290ed9e74405
scnakandala <supun.nakandala@gmail.com>
2016-10-05 04:24:43 -0400

    fixing path issue

--- modules/sharing-registry/thrift_models/thrift-gen.sh
+++ modules/sharing-registry/thrift_models/thrift-gen.sh
@@ -2,14 +2,14 @@
 
 thrift --gen java sharing_models.thrift
 cd gen-java
-rm -r ../../airavata-sharing-registry-stubs/src/main/java/org/apache/airavata/sharing/registry/models/*
-cp -r org/apache/airavata/sharing/registry/models/ ../../airavata-sharing-registry-stubs/src/main/java/org/apache/airavata/sharing/registry/models/
+rm -r ../../sharing-registry-stubs/src/main/java/org/apache/airavata/sharing/registry/models/*
+cp -r org/apache/airavata/sharing/registry/models/ ../../sharing-registry-stubs/src/main/java/org/apache/airavata/sharing/registry/models/
 
 cd ..
 thrift --gen java sharing_cpi.thrift
 cd gen-java
-rm -r ../../airavata-sharing-registry-stubs/src/main/java/org/apache/airavata/sharing/registry/service/cpi/*
-cp -r org/apache/airavata/sharing/registry/service/cpi/ ../../airavata-sharing-registry-stubs/src/main/java/org/apache/airavata/sharing/registry/service/cpi/
+rm -r ../../sharing-registry-stubs/src/main/java/org/apache/airavata/sharing/registry/service/cpi/*
+cp -r org/apache/airavata/sharing/registry/service/cpi/ ../../sharing-registry-stubs/src/main/java/org/apache/airavata/sharing/registry/service/cpi/
 
 cd ..
 
